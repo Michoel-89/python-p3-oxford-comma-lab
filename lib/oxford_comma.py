b35c36ca44f1d50d2249b378d9f6ea37f68d19db
michoel <michoelr111@gmail.com>
2023-06-13 13:51:59 -0400

    complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if(len(items) == 1):
+        return ''.join(items)
+    if(len(items) == 2):
+        items[-1] = ' and ' + items[-1]
+        return ''.join(items)
+    items[-1] = 'and ' + items[-1]
+    return ', '.join(items)
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))
